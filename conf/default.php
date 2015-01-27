1e7ede847f505603ff78f9875c8b814e99a054cd
Andreas Gohr <gohr@cosmocode.de>
2015-01-27 15:57:42 +0100

    wizard generated stub

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the tabbox plugin
+ *
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the tabbox plugin
+ *
+ * @author Andreas Gohr <dokuwiki@cosmocode.de>
+ */
+
+
+//$meta['fixme'] = array('string');
+
