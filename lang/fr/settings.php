6023fc28d4d632d34087b5046ae7b66b26cf1784
Laynee <seedfloyd@gmail.com>
2016-03-10 20:43:31 +0100

    French language file

---
+++ lang/fr/settings.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * french language file for tabbox plugin
+ *
+ * @author Laynee <seedfloyd@gmail.com>
+ */
+
+// keys need to match the config setting name
+$lang['tabs'] = 'Onglets par défaut insérés avec le bouton de la barre d\'outils (séparés par des virgules).';
+
+
+
+//Setup VIM: ex: et ts=4 :
